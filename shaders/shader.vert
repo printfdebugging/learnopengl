28004d3ab5eae6ede4cc92be088a6533f0bf43e5
Sahil Gautam <printfdebugging@gmail.com>
2025-11-10 08:11:42 +0530

    hello triangle (without any abstractions over mesh)

--- shaders/shader.vert
+++ shaders/shader.vert
@@ -1,5 +1,8 @@
 #version 330 core
 
+in vec4 in_position;
+
 void main()
 {
+    gl_Position = vec4(in_position.xyz, 1.0);
 }
--- source/main.c
+++ source/main.c
@@ -1,5 +1,6 @@
 #include "shader.h"
 #include "window.h"
+#include "glad/glad.h"
 
 #include <stdlib.h>
 
@@ -11,11 +12,42 @@ int main()
     struct shader* shader = shader_create_from_file("shaders/shader.vert", "shaders/shader.frag");
     if (!shader) return EXIT_FAILURE;
 
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    /* clang-format off */
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f,
+    };
+    /* clang-format on */
+
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+        MESH_ATTRIBUTE_POSITION,  // position of data in vao
+        3,                        // number of elements
+        GL_FLOAT,                 // type of data
+        GL_FALSE,                 // whether normalized or not
+        3 * sizeof(float),        // stride (offset between consecutive elements)
+        (void*) 0                 // offset of the first element from the start
+    );
+
+    glEnableVertexAttribArray(MESH_ATTRIBUTE_POSITION);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     while (!window_closed(window))
     {
         window_poll_events(window);
         window_process_input(window);
         window_clear_color(window);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         window_swap_buffers(window);
     }
