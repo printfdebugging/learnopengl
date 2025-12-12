db560f314c11c26afa5c19b4f57a615630c5e503
Sahil Gautam <printfdebugging@gmail.com>
2025-12-13 03:15:23 +0530

    merge back the cmake files

--- cmake/custom.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(VERSION 4.0)
-
-add_custom_target(run
-    COMMAND ${PROJECT_NAME} 
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    DEPENDS ${PROJECT_NAME} 
-    COMMENT "Running ${PROJECT_NAME} from root directory"
-)
